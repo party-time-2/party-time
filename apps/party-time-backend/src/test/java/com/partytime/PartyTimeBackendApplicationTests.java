7c32594fe5a31db442de119a7aa5b91aba8eed09
Julian Türner <julian@trner.de>
2023-04-05 18:24:10 +0000

    initial backend generation

---
+++ apps/party-time-backend/src/test/java/com/partytime/PartyTimeBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.partytime;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PartyTimeBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
