d3f87d120e53c4b2aafdf223cd5fb3c91ffac35e
Julian Türner <julian@trner.de>
2023-04-09 16:25:07 +0000

    added animation

--- libs/landing/src/lib/headline/headline.component.ts
+++ libs/landing/src/lib/headline/headline.component.ts
@@ -1,28 +1,35 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'party-time-headline',
   template: `
-    <h1
-      class="text-on-background-light dark:text-on-background-dark h-full text-3xl font-extrabold md:text-5xl lg:text-6xl"
-    >
-      Organisiere deine Party
-      <span
-        class="to-tertiary-light dark:to-tertiary-dark dark:from-primary-dark from-primary-light bg-gradient-to-r bg-clip-text text-transparent"
+    <section #headlineSection>
+      <h1
+        class="headline text-on-background-light dark:text-on-background-dark h-full text-3xl font-extrabold md:text-5xl lg:text-6xl"
       >
-        übersichtlich
-      </span>
-    </h1>
-    <p
-      class="text-on-surface-variant-light dark:text-on-surface-variant-dark mt-2 text-lg font-normal underline underline-offset-4 sm:mt-3 md:mt-4 lg:mt-6 lg:text-xl"
-    >
-      Du machst Party.
-    </p>
-    <p
-      class="text-on-surface-variant-light dark:text-on-surface-variant-dark text-lg font-normal lg:text-xl"
-    >
-      Wir behalten den Überblick.
-    </p>
+        Organisiere deine Party
+        <span
+          class="to-tertiary-light dark:to-tertiary-dark dark:from-primary-dark from-primary-light bg-gradient-to-r bg-clip-text text-transparent"
+        >
+          übersichtlich
+        </span>
+      </h1>
+      <div class="subline">
+        <p
+          class="text-on-surface-variant-light dark:text-on-surface-variant-dark mt-2 text-lg font-normal underline underline-offset-4 sm:mt-3 md:mt-4 lg:mt-6 lg:text-xl"
+        >
+          Du machst Party.
+        </p>
+        <p
+          class="text-on-surface-variant-light dark:text-on-surface-variant-dark text-lg font-normal lg:text-xl"
+        >
+          Wir behalten den Überblick.
+        </p>
+      </div>
+    </section>
   `,
 })
-export class HeadlineComponent {}
+export class HeadlineComponent {
+  @ViewChild('headlineSection')
+  headlineSection!: ElementRef;
+}
--- libs/landing/src/lib/landing-page/landing-page.component.ts
+++ libs/landing/src/lib/landing-page/landing-page.component.ts
@@ -1,4 +1,14 @@
-import { Component } from '@angular/core';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/all';
+import { HeadlineComponent } from '../headline/headline.component';
+gsap.registerPlugin(ScrollTrigger);
 
 @Component({
   selector: 'party-time-landing-page',
@@ -11,6 +21,7 @@ import { Component } from '@angular/core';
       <party-time-headline></party-time-headline>
       <div class="max-w pt-4 sm:pt-6 md:pt-10 lg:pt-16">
         <img
+          #partyImage
           class="rounded-xl object-cover shadow-xl"
           src="https://images.pexels.com/photos/3171837/pexels-photo-3171837.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
           alt="Bild von einer Party"
@@ -20,4 +31,40 @@ import { Component } from '@angular/core';
   </section> `,
   styles: [],
 })
-export class LandingPageComponent {}
+export class LandingPageComponent implements AfterViewInit {
+  @ViewChild(HeadlineComponent) headlineComponent!: HeadlineComponent;
+  @ViewChild('headline')
+  headline!: HTMLElement;
+  @ViewChild('subline')
+  subline!: HTMLElement;
+
+  constructor(private elementRef: ElementRef) {}
+
+  ngAfterViewInit() {
+    this.headline =
+      this.headlineComponent.headlineSection.nativeElement.querySelector(
+        '.headline'
+      );
+    this.subline =
+      this.headlineComponent.headlineSection.nativeElement.querySelector(
+        '.subline'
+      );
+    this.animateTimeline();
+  }
+
+  animateTimeline() {
+    gsap
+      .timeline()
+      .from(this.headline, {
+        duration: 1,
+        y: 100,
+        opacity: 0,
+        ease: 'power4.out',
+      })
+      .from(
+        this.subline,
+        { duration: 1, x: -100, opacity: 0, ease: 'power3.out' },
+        '-=0.5'
+      );
+  }
+}
