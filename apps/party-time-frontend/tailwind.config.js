584fa8b591870eb21e4b83e01edf7aedab667b37
Julian Turner <33830803+JulianTurner@users.noreply.github.com>
2023-04-10 18:10:59 +0200

    Update tailwind.config.js

--- apps/party-time-frontend/tailwind.config.js
+++ apps/party-time-frontend/tailwind.config.js
@@ -89,6 +89,6 @@ module.exports = {
       'scrim-dark': withOpacity('--scrim-dark'),
     },
   },
-  darkMode: 'class', // or 'media' or 'class'
+  darkMode: 'media', // or 'media' or 'class'
   plugins: [],
 };
