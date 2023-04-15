90ff5c5c12d1263d55136c0e234ad72a193c3d42
Julian Türner <julian@trner.de>
2023-04-15 18:25:09 +0000

    make projects pass test

--- libs/models/jest.config.ts
+++ libs/models/jest.config.ts
@@ -3,7 +3,6 @@ export default {
   displayName: 'models',
   preset: '../../jest.preset.js',
   setupFilesAfterEnv: ['<rootDir>/src/test-setup.ts'],
-  coverageDirectory: '../../coverage/libs/models',
   transform: {
     '^.+\\.(ts|mjs|js|html)$': [
       'jest-preset-angular',
