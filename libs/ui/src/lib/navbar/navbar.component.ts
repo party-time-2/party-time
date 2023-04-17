86288d2832301b51d4cd690660da938be1fcf42b
Julian Türner <julian@trner.de>
2023-04-17 11:34:13 +0000

    adujted navbar size

--- libs/ui/src/lib/navbar/navbar.component.ts
+++ libs/ui/src/lib/navbar/navbar.component.ts
@@ -10,7 +10,7 @@ import { PrimaryButtonComponent } from '../primary-button/primary-button.compone
   standalone: true,
   imports: [CommonModule, RouterModule, LogoComponent, PrimaryButtonComponent],
   template: `
-    <nav class="px-2 py-2.5 sm:px-4 ">
+    <nav class="w-full max-w-full ">
       <div
         class="container mx-auto flex flex-wrap items-center justify-between"
       >
