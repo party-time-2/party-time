4ceddea5fee792d57d0e980dae60ca45f343ad3f
Julian Türner <julian@trner.de>
2023-04-21 11:07:13 +0000

    removed unused e2e

--- apps/party-time-frontend-e2e/src/e2e/app.component.cy.ts
+++
@@ -1,6 +0,0 @@
-describe('party-time-frontend', () => {
-  beforeEach(() => cy.visit('/iframe.html?id=appcomponent--primary'));
-  it('should render the component', () => {
-    cy.get('party-time-root').should('exist');
-  });
-});
