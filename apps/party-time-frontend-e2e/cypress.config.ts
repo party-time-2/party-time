e092812c7c898a69210b18ff37d8fc34d1272cae
Julian Türner <julian@trner.de>
2023-04-23 19:01:16 +0000

    added key

--- apps/party-time-frontend-e2e/cypress.config.ts
+++ apps/party-time-frontend-e2e/cypress.config.ts
@@ -2,11 +2,8 @@ import { defineConfig } from 'cypress';
 import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
 
 export default defineConfig({
-  projectId: 'tqvzz3',
   e2e: {
     ...nxE2EPreset(__dirname),
-    // supportFolder: 'apps/party-time-frontend-e2e/src/support',
-    // supportFile: false,
-    supportFile: false,
+    projectId: 'tqvzz3',
   },
 });
