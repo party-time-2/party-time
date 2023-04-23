d7a72b38d09940d371c283f6d5a8eb0b2ef80411
Julian Türner <julian@trner.de>
2023-04-23 18:17:33 +0000

    sdfgj;ds

--- apps/party-time-frontend-e2e/cypress.config.ts
+++ apps/party-time-frontend-e2e/cypress.config.ts
@@ -3,6 +3,10 @@ import { nxE2EPreset } from '@nrwl/cypress/plugins/cypress-preset';
 
 export default defineConfig({
   projectId: 'tqvzz3',
-  e2e: nxE2EPreset(__dirname),
-  supportFolder: 'apps/party-time-frontend-e2e/src/support',
+  e2e: {
+    ...nxE2EPreset(__dirname),
+    // supportFolder: 'apps/party-time-frontend-e2e/src/support',
+    // supportFile: false,
+    supportFile: false,
+  },
 });
