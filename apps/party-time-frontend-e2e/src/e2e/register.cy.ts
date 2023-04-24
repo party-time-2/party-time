cd4d107f8bd1480539d718c398e6ade391fb543e
Julian Türner <julian@trner.de>
2023-04-24 10:41:37 +0000

    frontend test for registration

--- apps/party-time-frontend-e2e/src/e2e/register.cy.ts
+++ apps/party-time-frontend-e2e/src/e2e/register.cy.ts
@@ -1,75 +1,75 @@
 import { navigateRegister } from '../support/app.po';
 
-// describe('party-time-register-error-user', () => {
-//   it('should show user_too_short', () => {
-//     navigateRegister();
-//     cy.get('#name').type('a');
-//     cy.contains('Der Benutzername muss mindestens 5 Zeichen lang sein.');
-//     cy.screenshot();
-//   });
-//   it('should show user_too_long', () => {
-//     navigateRegister();
-//     cy.get('#name').type(
-//       'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
-//     );
-//     cy.contains('Der Benutzername darf maximal 30 Zeichen lang sein.');
-//     cy.screenshot();
-//   });
-//   it('should show user_required', () => {
-//     navigateRegister();
-//     cy.get('#name').type('a');
-//     cy.get('#name').clear();
-//     cy.contains('Bitte gib einen Benutzernamen ein.');
-//     cy.screenshot();
-//   });
-// });
+describe('party-time-register-error-user', () => {
+  it('should show user_too_short', () => {
+    navigateRegister();
+    cy.get('#name').type('a');
+    cy.contains('Der Benutzername muss mindestens 5 Zeichen lang sein.');
+    cy.screenshot();
+  });
+  it('should show user_too_long', () => {
+    navigateRegister();
+    cy.get('#name').type(
+      'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa'
+    );
+    cy.contains('Der Benutzername darf maximal 30 Zeichen lang sein.');
+    cy.screenshot();
+  });
+  it('should show user_required', () => {
+    navigateRegister();
+    cy.get('#name').type('a');
+    cy.get('#name').clear();
+    cy.contains('Bitte gib einen Benutzernamen ein.');
+    cy.screenshot();
+  });
+});
 
-// describe('party-time-register-error-mail', () => {
-//   it('should show mail_required', () => {
-//     navigateRegister();
-//     cy.get('#email').type('a');
-//     cy.get('#email').clear();
-//     cy.contains(
-//       'Bitte gib deine E-Mail Adresse ein damit wir verifzieren können dass du eine echte Person bist und du zur Not dein Passwort zurück setzen kannst.'
-//     );
-//     cy.screenshot();
-//   });
-//   it('should show mail_invalid', () => {
-//     navigateRegister();
-//     cy.get('#email').type('a');
-//     cy.contains(
-//       'Bitte gib eine Valide E-Mail Adresse ein damit wir verifzieren können dass du eine echte Person bist. Es wird eine Verfizierung an diese Adresse gesendet.'
-//     );
-//     cy.screenshot();
-//   });
-// });
+describe('party-time-register-error-mail', () => {
+  it('should show mail_required', () => {
+    navigateRegister();
+    cy.get('#email').type('a');
+    cy.get('#email').clear();
+    cy.contains(
+      'Bitte gib deine E-Mail Adresse ein damit wir verifzieren können dass du eine echte Person bist und du zur Not dein Passwort zurück setzen kannst.'
+    );
+    cy.screenshot();
+  });
+  it('should show mail_invalid', () => {
+    navigateRegister();
+    cy.get('#email').type('a');
+    cy.contains(
+      'Bitte gib eine Valide E-Mail Adresse ein damit wir verifzieren können dass du eine echte Person bist. Es wird eine Verfizierung an diese Adresse gesendet.'
+    );
+    cy.screenshot();
+  });
+});
 
-// describe('party-time-register-error-pw', () => {
-//   it('should show pw_required', () => {
-//     navigateRegister();
-//     cy.get('#password').type('a');
-//     cy.get('#password').clear();
-//     cy.contains('Bitte gib ein Passwort ein.');
-//     cy.screenshot();
-//   });
-//   it('should show pw_short', () => {
-//     navigateRegister();
-//     cy.get('#password').type('a');
-//     cy.contains('Das Passwort muss mindestens 8 Zeichen lang sein.');
-//     cy.screenshot();
-//   });
-//   it('should show pw_long', () => {
-//     navigateRegister();
-//     cy.get('#password').type('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
-//     cy.contains('Das Passwort darf maximal 30 Zeichen lang sein.');
-//     cy.screenshot();
-//   });
-//   it('should show pw_wrong_chars', () => {
-//     navigateRegister();
-//     cy.get('#password').type('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
-//     cy.contains('Das Passwort muss mindestens 1 Sonderzeichen');
-//   });
-// });
+describe('party-time-register-error-pw', () => {
+  it('should show pw_required', () => {
+    navigateRegister();
+    cy.get('#password').type('a');
+    cy.get('#password').clear();
+    cy.contains('Bitte gib ein Passwort ein.');
+    cy.screenshot();
+  });
+  it('should show pw_short', () => {
+    navigateRegister();
+    cy.get('#password').type('a');
+    cy.contains('Das Passwort muss mindestens 8 Zeichen lang sein.');
+    cy.screenshot();
+  });
+  it('should show pw_long', () => {
+    navigateRegister();
+    cy.get('#password').type('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+    cy.contains('Das Passwort darf maximal 30 Zeichen lang sein.');
+    cy.screenshot();
+  });
+  it('should show pw_wrong_chars', () => {
+    navigateRegister();
+    cy.get('#password').type('aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa');
+    cy.contains('Das Passwort muss mindestens 1 Sonderzeichen');
+  });
+});
 
 describe('party-time-register', () => {
   const dateTime = Date.now().toString();
