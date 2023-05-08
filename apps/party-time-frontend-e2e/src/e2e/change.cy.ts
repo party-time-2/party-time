3c356ba69e449cb038d0cfe3871ace857847c48d
Julian Türner <julian@trner.de>
2023-05-08 13:19:35 +0000

    add test screenshots

--- apps/party-time-frontend-e2e/src/e2e/change.cy.ts
+++ apps/party-time-frontend-e2e/src/e2e/change.cy.ts
@@ -9,48 +9,48 @@ import {
   pw_valid,
 } from '../support/app.po';
 
-// describe('party-time-change-error', () => {
-//   beforeEach(() => {
-//     cy.login();
-//     navigateChange();
-//   });
+describe('party-time-change-error', () => {
+  beforeEach(() => {
+    cy.login();
+    navigateChange();
+  });
 
-//   it('should show pw_old_required', () => {
-//     cy.get('#current-password').type(pw_short());
-//     cy.get('#current-password').clear();
-//     cy.contains('Bitte gib dein aktuelles Passwort ein.');
-//     cy.screenshot();
-//     // cy.get('#new-password');
-//     // cy.get('party-time-primary-button > #change')
-//   });
+  it('should show pw_old_required', () => {
+    cy.get('#current-password').type(pw_short());
+    cy.get('#current-password').clear();
+    cy.contains('Bitte gib dein aktuelles Passwort ein.');
+    cy.screenshot();
+    // cy.get('#new-password');
+    // cy.get('party-time-primary-button > #change')
+  });
 
-//   it('should show pw_new_required', () => {
-//     cy.get('#new-password').type(pw_short());
-//     cy.get('#new-password').clear();
-//     cy.contains(pw_required_error());
-//     cy.screenshot();
-//   });
+  it('should show pw_new_required', () => {
+    cy.get('#new-password').type(pw_short());
+    cy.get('#new-password').clear();
+    cy.contains(pw_required_error());
+    cy.screenshot();
+  });
 
-//   it('should show pw_new_short', () => {
-//     cy.get('#new-password').type(pw_short());
-//     cy.get('#new-password').clear();
-//     cy.get('#new-password').type(pw_short());
-//     cy.contains(pw_short_error());
-//     cy.screenshot();
-//   });
+  it('should show pw_new_short', () => {
+    cy.get('#new-password').type(pw_short());
+    cy.get('#new-password').clear();
+    cy.get('#new-password').type(pw_short());
+    cy.contains(pw_short_error());
+    cy.screenshot();
+  });
 
-//   it('should show pw_new_long', () => {
-//     cy.get('#new-password').type(pw_long());
-//     cy.contains(pw_long_error());
-//     cy.screenshot();
-//   });
+  it('should show pw_new_long', () => {
+    cy.get('#new-password').type(pw_long());
+    cy.contains(pw_long_error());
+    cy.screenshot();
+  });
 
-//   it('should show pw_new_wrong_chars', () => {
-//     cy.get('#new-password').type(pw_short());
-//     cy.contains(pw_wrong_chars_error());
-//     cy.screenshot();
-//   });
-// });
+  it('should show pw_new_wrong_chars', () => {
+    cy.get('#new-password').type(pw_short());
+    cy.contains(pw_wrong_chars_error());
+    cy.screenshot();
+  });
+});
 
 describe('party-time-change', () => {
   beforeEach(() => {
