4e6b4446a08095961b8c01db7cff85959f8a1142
dangllucas <dangllucas@gmail.com>
2023-05-19 00:02:32 +0200

    Update apps/party-time-backend/src/main/java/com/partytime/api/controller/EventController.java
    Co-authored-by: E3FxGaming <8276268+E3FxGaming@users.noreply.github.com>

--- apps/party-time-backend/src/main/java/com/partytime/api/controller/EventController.java
+++ apps/party-time-backend/src/main/java/com/partytime/api/controller/EventController.java
@@ -130,7 +130,7 @@ public class EventController {
         }
     )
     public void uninviteParticipant(@Parameter(description = "The id of the event") @PathVariable("id") Long eventId,
-                                    @Parameter(description = "The Email of the Guest to invite") @PathVariable("email") String email,
+                                    @Parameter(description = "The e-mail of the guest to invite") @PathVariable("email") String email,
                                     TokenAuthentication authentication) {
         // TODO Implementation
     }
