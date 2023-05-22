208634814d363a03cd9a21f345d6560dfe2ff7eb
Julian Türner <julian@trner.de>
2023-05-22 09:19:09 +0000

    add service and state

--- libs/models/src/lib/api/event.model.ts
+++ libs/models/src/lib/api/event.model.ts
@@ -1,4 +1,14 @@
+import { AccountDTO } from './account.model';
+
 export interface EventDTO {
+  id: string;
+  name: string;
+  organizer: AccountDTO;
+  dateTime: string;
+  address: Address;
+}
+
+export interface EventCreateDTO {
   name: string;
   dateTime: string;
   address: Address;
