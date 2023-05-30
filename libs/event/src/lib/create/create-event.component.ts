4c69c6c64e1120079f9e5956fec902b4bf79a0c2
Julian Türner <julian@trner.de>
2023-05-30 13:31:55 +0000

    add seconds to form

--- libs/event/src/lib/create/create-event.component.ts
+++ libs/event/src/lib/create/create-event.component.ts
@@ -69,7 +69,7 @@ export class CreateEventComponent {
     }),
 
     dateTime: new FormControl(
-      this.datePipe.transform(new Date() as Date, 'yyyy-MM-dd HH:mm'),
+      this.datePipe.transform(new Date() as Date, 'yyyy-MM-dd HH:mm:ss'),
       Validators.required
     ),
   });
