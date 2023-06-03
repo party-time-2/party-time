e7f33e32c3a7d587b7169951438b7696e5bffe39
Julian Türner <julian@trner.de>
2023-06-03 14:20:27 +0000

    add header

--- libs/event/src/lib/services/event.service.ts
+++ libs/event/src/lib/services/event.service.ts
@@ -1,4 +1,5 @@
 //implements F001
+//implements F016
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { EventCreateDTO, EventDTO } from '@party-time/models';
