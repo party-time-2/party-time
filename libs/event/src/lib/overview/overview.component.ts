285eb0c57267a2f3ebaed470edbdec3e14707c38
Julian Türner <julian@trner.de>
2023-06-03 14:23:25 +0000

    add component logic and template file

--- libs/event/src/lib/overview/overview.component.ts
+++ libs/event/src/lib/overview/overview.component.ts
@@ -1,11 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { MainHeaderComponent, PrimaryButtonComponent, PrimaryErrorComponent, PrimaryLabelComponent } from '@party-time/ui';
+import { OverviewStore } from './+state/overview.state';
 
 @Component({
   selector: 'party-time-overview',
   standalone: true,
-  imports: [CommonModule],
-  template: `<p>overview works!</p>`,
+  imports: [CommonModule, PrimaryButtonComponent, MainHeaderComponent, PrimaryLabelComponent, PrimaryErrorComponent],
+  templateUrl: './overview.component.html',
+  providers: [OverviewStore],
   styles: [],
 })
-export class OverviewComponent {}
+export class OverviewComponent {
+  vm$ = this.overviewStore.vm$;
+
+  constructor(private overviewStore: OverviewStore) {
+    this.overviewStore.getEvents();
+  }
+}
