0f51575d7d78e8a44041164f8b42c2460fa73eec
Julian Türner <julian@trner.de>
2023-07-19 17:51:19 +0000

    format repo

--- libs/invitation/src/lib/services/invitation.service.ts
+++ libs/invitation/src/lib/services/invitation.service.ts
@@ -3,17 +3,21 @@ import { Injectable } from '@angular/core';
 
 @Injectable()
 export class InvitationService {
-
   constructor(private http: HttpClient) {}
 
   eventBasePath = '/api/event';
 
   acceptInvitation(eventId: string) {
-    return this.http.post(`${this.eventBasePath}/${eventId}/participants/invitation/accept`, {});
+    return this.http.post(
+      `${this.eventBasePath}/${eventId}/participants/invitation/accept`,
+      {}
+    );
   }
 
   declineInvitation(eventId: string) {
-    return this.http.post(`${this.eventBasePath}/${eventId}/participants/invitation/decline`, {});
+    return this.http.post(
+      `${this.eventBasePath}/${eventId}/participants/invitation/decline`,
+      {}
+    );
   }
-
 }
