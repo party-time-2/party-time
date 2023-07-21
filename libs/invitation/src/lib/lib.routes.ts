810dd413de505b5bc8515cb4faf420afe989374f
E3FxGaming <8276268+E3FxGaming@users.noreply.github.com>
2023-07-21 16:32:59 +0200

    revert 3b294a48

--- libs/invitation/src/lib/lib.routes.ts
+++
@@ -1,6 +0,0 @@
-import { Route } from '@angular/router';
-import { InvitationComponent } from './invitation/invitation.component';
-
-export const invitationRoutes: Route[] = [
-  { path: '', component: InvitationComponent },
-];
