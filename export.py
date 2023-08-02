12473b514f53df91a9b95ceba12339ead74595f1
E3FxGaming <8276268+E3FxGaming@users.noreply.github.com>
2023-08-02 08:00:06 +0000

    export.py overhaul 2

--- export.py
+++ export.py
@@ -6,11 +6,17 @@ from datetime import datetime
 from glob import glob
 from pathlib import Path
 import re
+from argparse import ArgumentParser
 
 step_count = 1
 script_dir = Path(__file__).absolute().parent
 
 def copy_to_temp_dir(temp_Path: Path):
+    """Copy files described in export.json from the working directory to a temp directory
+
+    Args:
+        temp_Path (Path): destination path of the copy operation
+    """
     global step_count
     print(f"Step {step_count}: copy files to temporary directory")
     step_count += 1
@@ -40,6 +46,14 @@ def copy_to_temp_dir(temp_Path: Path):
                 print()
 
 def check_glossary(glossary_root: Path) -> bool:
+    """Checks the glossary for unused/undefined terms
+
+    Args:
+        glossary_root (Path): path below which to look for the glossary.md and design_entscheidungen.md
+
+    Returns:
+        bool: overall success of the operation - may return false if there are unused/undefined terms in the glossary
+    """
     global step_count
     print(f"Step {step_count}: check glossary definitions")
     step_count += 1
@@ -65,7 +79,7 @@ def check_glossary(glossary_root: Path) -> bool:
         # determine defined entries
         defined_terms = re.findall(r"(?<=## ).*?(?=$)", glossar_content, re.MULTILINE)
         defined_terms = set([x.lower() for x in defined_terms])
-        print(f"{len(defined_terms)} glossary terms found")
+        print(f"{len(defined_terms)} defined glossary terms found")
         print("")
 
         # compare entry sets
@@ -92,10 +106,20 @@ def check_glossary(glossary_root: Path) -> bool:
         print(f'{glossary_Path}: {"exists" if glossary_Path.exists() else "missing"}')
         return False
     
-def md_to_pdf(md_files_root_Path: Path):
+def md_to_pdf(md_files_root_Path: Path) -> bool:
+    """Transforms markdown files into pdf files and deletes markdown files
+
+    Args:
+        md_files_root_Path (Path): root directory in which to look for markdown files (recursively)
+
+    Returns:
+        bool: overall success of the operation - operations may fail if pandoc reports problems
+    """
     global step_count
     print(f"Step {step_count}: convert .md files to .pdf")
     step_count += 1
+
+    overall_success = True
 
     global script_dir
     md_files = sorted(md_files_root_Path.glob('**/*.md'))
@@ -105,10 +129,19 @@ def md_to_pdf(md_files_root_Path: Path):
             src_path = Path(src_str)
             dest_path = src_path.with_suffix(".pdf")
             print(f"({index+1}/{len(md_files)}): Converting {src_path.relative_to(md_files_root_Path)} -> {dest_path.relative_to(md_files_root_Path)}")
-            subprocess.run(["/usr/bin/pandoc", src_path, "-o", dest_path, "--lua-filter", lua_script_path], cwd=temp_Path)
+            subprocess_res = subprocess.run(["/usr/bin/pandoc", src_path, "-o", dest_path, "--lua-filter", lua_script_path], cwd=temp_Path)
+            if subprocess_res.returncode != 0:
+                overall_success = False
             src_path.unlink()
+    print(f"Overall success: {overall_success}")
+    return overall_success
 
 def zip_result(temp_Path: Path):
+    """zips the content of a directory to an output zip archive into the working directory
+
+    Args:
+        temp_Path (Path): path whose content should be zipped
+    """
     global step_count
     print(f"Step {step_count}: zip result")
     step_count += 1
@@ -121,17 +154,40 @@ def zip_result(temp_Path: Path):
     print(f"Export output written to {export_output_name}.zip")
 
 
-with TemporaryDirectory(prefix="party-time-") as tempdirname:
-    temp_Path = Path(tempdirname)
+parser = ArgumentParser(
+    prog="Project export helper",
+    description="Utility tool for exporting project data"
+)
 
-    print(f"created temporary directory {temp_Path}")
+parser.add_argument("-c", "--check-glossary",
+                    dest="glossary_only", action="store_true",
+                    help="Only checks the glossary for missing/unused terms")
 
-    copy_to_temp_dir(temp_Path)
-    print()
+args = parser.parse_args()
 
-    check_glossary(temp_Path)
-    print()
+if args.glossary_only:
+    check_glossary_success = check_glossary(script_dir)
+    if not check_glossary_success:
+        exit(1)
+else:
+    export_success = True
 
-    md_to_pdf(temp_Path)
+    with TemporaryDirectory(prefix="party-time-") as tempdirname:
+        temp_Path = Path(tempdirname)
 
-    zip_result(temp_Path)
+        print(f"created temporary directory {temp_Path}")
+
+        copy_to_temp_dir(temp_Path)
+        print()
+
+        if not check_glossary(temp_Path):
+            export_success = False
+        print()
+
+        if not md_to_pdf(temp_Path):
+            export_success = False
+        print()
+
+        zip_result(temp_Path)
+    if not export_success:
+        exit(1)
