8f3f3121312c4d5b125ef146df5e4b99ad53bf5a
E3FxGaming <8276268+E3FxGaming@users.noreply.github.com>
2023-08-08 15:24:31 +0000

    format repo

--- .storybook/main.ts
+++ .storybook/main.ts
@@ -1,6 +1,6 @@
 module.exports = {
   stories: [],
-  addons: ['@storybook/addon-essentials']
+  addons: ['@storybook/addon-essentials'],
   // uncomment the property below if you want to apply some webpack config globally
   // webpackFinal: async (config, { configType }) => {
   //   // Make whatever fine-grained changes you need that should apply to all storybook configs
@@ -8,13 +8,11 @@ module.exports = {
   //   // Return the altered config
   //   return config;
   // },
-  ,
-
   framework: {
     name: '@storybook/angular',
-    options: {}
+    options: {},
   },
   docs: {
-    autodocs: true
-  }
+    autodocs: true,
+  },
 };
