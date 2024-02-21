9c89beb5ee6f1f7b4e3ebcd1a863f9a871a77b57
Julian Turner <tuerner.j@gmail.com>
2024-02-21 10:34:45 +0100

    add environment files

---
+++ apps/party-time-frontend-17/src/environments/environment.development.ts
@@ -0,0 +1,18 @@
+export const environment = {
+  api:
+    {
+      baseUrl: 'http://localhost:8090/api',
+      endpoints: {
+        authentication: {
+          login: '/auth/login',
+          register: '/auth/register',
+          verifyEmail: '/auth/verify',
+        },
+        account: {
+          delete: '/account/delete',
+          change: "/auth/change",
+        },
+          event: '/api/event',
+      }
+    }
+};
