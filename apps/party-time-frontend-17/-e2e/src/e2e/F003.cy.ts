2cbc815d6153b677a2ea84b2964abbbd7de08d35
Julian Turner <julian@trner.de>
2024-06-05 11:57:31 +0000

    f003 cypress files

--- apps/party-time-frontend-17/-e2e/src/e2e/F003.cy.ts
+++ apps/party-time-frontend-17/-e2e/src/e2e/F003.cy.ts
@@ -1,37 +1,37 @@
 import { getScreenshotPath } from '../support/utils';
 
-const insert_group = 'worteingabe';
-const error_group = 'worteingabe-error';
-describe(error_group, () => {
+const requiremnt = "F003";
+const event_group = 'delete-event-success';
+describe(event_group, () => {
   beforeEach(() => {
     cy.visit('/');
   });
 
-  const show_letter_input_test = 'should show letter_input';
-  it(show_letter_input_test, () => {
-    cy.get('[data-cy="letter-0"]').clear();
-    cy.get('[data-cy="letter-0"]').type('A');
-    cy.get('[data-cy="letter-0"]').should('have.value', 'A');
-
+  const delete_event_success_test = 'should show delete-event-success';
+  it(delete_event_success_test, () => {
 
     cy.screenshot(
-      getScreenshotPath('F001', insert_group, show_letter_input_test),
+      getScreenshotPath(requiremnt, event_group, delete_event_success_test),
       {
         overwrite: true,
       }
     );
   });
+});
 
-  const show_non_letter_input = 'should show non_letter_input';
-  it(show_non_letter_input, () => {
-    cy.get('[data-cy="letter-0"]').clear();
-    cy.get('[data-cy="letter-0"]').type('1');
-    cy.get('[data-cy="letter-0"]').should('have.value', ''); // input field should be empty
-    cy.get('[data-cy="guess-button"]').should('be.disabled'); // guess button should be disabled
+
+const change_event_error_group = 'delete-event-error';
+describe(change_event_error_group, () => {
+  beforeEach(() => {
+    cy.visit('/');
+  });
+
+  const delete_event_error_test = 'should show delete-event-error';
+  it(delete_event_error_test, () => {
 
 
     cy.screenshot(
-      getScreenshotPath('F001', error_group, show_non_letter_input),
+      getScreenshotPath(requiremnt, change_event_error_group, delete_event_error_test),
       {
         overwrite: true,
       }
