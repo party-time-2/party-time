e593a9d20a337e6db003f24c96ce8091db744ddb
Julian Turner <julian@trner.de>
2024-06-05 08:20:02 +0000

    docs refactoring

--- .devcontainer/postCreateCommand.sh
+++ .devcontainer/postCreateCommand.sh
@@ -1,3 +1,4 @@
 npm install -g prettier
 npm install
 ./node_modules/.bin/cypress install
+wget https://github.com/plantuml/plantuml/releases/download/v1.2024.5/plantuml-1.2024.5.jar -O plantuml-release.jar
