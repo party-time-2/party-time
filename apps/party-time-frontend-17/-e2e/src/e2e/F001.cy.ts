7a90619426bae91cf5ad4cdc4618980691d8fa6d
Julian Turner <julian@trner.de>
2024-07-03 18:10:40 +0000

    starting with e2e

--- apps/party-time-frontend-17/-e2e/src/e2e/F001.cy.ts
+++ apps/party-time-frontend-17/-e2e/src/e2e/F001.cy.ts
@@ -5,12 +5,17 @@ const requirement = 'F001';
 const createEventGroup = 'Events anlegen';
 describe(createEventGroup, () => {
   beforeEach(() => {
-    cy.visit('/');
+    cy.login();
+    cy.visit('/events');
   });
 
   const generateEvent =
     'Veranstalter sollten in der Lage sein, ein neues Event anzulegen, indem sie ein Formular ausfüllen, in dem sie alle relevanten Informationen zum Event (Name des Events, Datum, Uhrzeit, Veranstaltungsort) angeben.';
   it(generateEvent, () => {
+    cy.get('.flex-row > .mdc-fab > .mat-mdc-button-touch-target').click();
+cy.get('[data-cy="event-name-input"]').type('Test Event');
+
+
     cy.screenshot(
       getScreenshotPath(requirement, createEventGroup, generateEvent),
       {
--- apps/party-time-frontend-17/-e2e/src/support/commands.ts
+++ apps/party-time-frontend-17/-e2e/src/support/commands.ts
@@ -14,14 +14,20 @@
 declare namespace Cypress {
   // eslint-disable-next-line @typescript-eslint/no-unused-vars
   interface Chainable<Subject> {
-    login(email: string, password: string): void;
+       login(): void;
   }
 }
 
-// -- This is a parent command --
-Cypress.Commands.add('login', (email, password) => {
-  console.log('Custom command example: Login', email, password);
+Cypress.Commands.add('login', () => {
+  cy.window().then((window) => {
+    window.localStorage.setItem(
+      'auth_token',
+      'eyJhbGciOiJIUzI1NiJ9.eyJqdGkiOiJjY2EyZmU3Mi1kODA1LTRiZmYtODQwNy04NzU5Zjc3MzE5OTciLCJpYXQiOjE2ODYzNzcxMjEsImlzcyI6Imh0dHBzOi8vcGFydHl0aW1lLmRlL2F1dGgiLCJzdWIiOiJmNDY3MDRkMi04ZTI5LTRhYzUtYWRjNi01YzE5OTA1NWM2MmMiLCJlbWFpbCI6InZlcmlmaWVkMUBwYXJ0eXRpbWUuZGUiLCJuYW1lIjoiVmVyaWZpZWQgVXNlciAxIiwiZW1haWxfdmVyaWZpZWQiOnRydWV9.y54PV0tS2SH61d9e5n2OnraeeRhTByr0T3twiK4Q9bc'
+    );
+  });
 });
+
+
 //
 // -- This is a child command --
 // Cypress.Commands.add("drag", { prevSubject: 'element'}, (subject, options) => { ... })
