4e69cdb91e1abe7cd3d4a15590e76d7250594fea
Julian Turner <julian@trner.de>
2024-07-04 12:01:38 +0000

    add cypress

--- apps/party-time-frontend-17/-e2e/src/e2e/F016.cy.ts
+++ apps/party-time-frontend-17/-e2e/src/e2e/F016.cy.ts
@@ -4,14 +4,17 @@ const requirement = 'F016';
 const eventsSummary = 'Events überblicken';
 describe(eventsSummary, () => {
   beforeEach(() => {
-    cy.visit('/');
+       cy.viewport(1920, 1080);
+    cy.login();
+    cy.visit('/events');
+  
   });
 
   const ownEventsListPageTest =
     'Die Plattform sollte eine Liste von allen eigenen Events auf einer Seite bereitstellen.';
   it(ownEventsListPageTest, () => {
     cy.screenshot(
-      getScreenshotPath(requirement, eventsSummary, ownEventsListPageTest),
+      getScreenshotPath(requirement, eventsSummary, "1"),
       {
         overwrite: true,
       }
@@ -21,8 +24,11 @@ describe(eventsSummary, () => {
   const eventSummary =
     'Die Liste sollte die wichtigsten Informationen über das Event, wie den Namen, das Datum, die Uhrzeit und den Veranstaltungsort, enthalten.';
   it(eventSummary, () => {
-    cy.screenshot(getScreenshotPath(requirement, eventsSummary, eventSummary), {
-      overwrite: true,
-    });
+cy.screenshot(
+      getScreenshotPath(requirement, eventsSummary, "2"),
+      {
+        overwrite: true,
+      }
+    );
   });
 });
