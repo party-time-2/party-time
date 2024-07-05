02b69dafed1f8201cbea9308c08028396a51153d
Julian Turner <julian@trner.de>
2024-07-05 15:32:07 +0000

    tests

--- apps/party-time-frontend-17/-e2e/src/e2e/F015.cy.ts
+++ apps/party-time-frontend-17/-e2e/src/e2e/F015.cy.ts
@@ -13,31 +13,21 @@ describe(deleteAccount, () => {
     'Benutzer sollten in der Lage sein, ihr Konto durch Ausfüllen eines Formulars auf der Plattform zu löschen.';
   it(accountDeletionSuccessTest, () => {
     cy.get('[data-cy="password-input"]').type('Hallo123!party');
-    cy.screenshot(
-      getScreenshotPath(requirement, deleteAccount,  '1'),
-      {
-        overwrite: true,
-        capture: 'viewport',
-      }
-    );
+    cy.screenshot(getScreenshotPath(requirement, deleteAccount, '1'), {
+      overwrite: true,
+      capture: 'viewport',
+    });
   });
   const accountDeletionConfirmationError =
     'Das Formular sollte eine Bestätigungsoption für den Benutzer enthalten, um sicherzustellen, dass das Löschen des Kontos beabsichtigt ist.';
   it(accountDeletionConfirmationError, () => {
     cy.get('[data-cy="password-input"]').type('Hallo123!party');
     cy.get('[data-cy="delete-account-button"]').click();
- // eslint-disable-next-line cypress/no-unnecessary-waiting
- cy.wait(300);
-    cy.screenshot(
-      getScreenshotPath(
-        requirement,
-        deleteAccount,
-        '2'
-      ),
-      {
-        overwrite: true,
-        capture: 'viewport',
-      }
-    );
+    // eslint-disable-next-line cypress/no-unnecessary-waiting
+    cy.wait(300);
+    cy.screenshot(getScreenshotPath(requirement, deleteAccount, '2'), {
+      overwrite: true,
+      capture: 'viewport',
+    });
   });
 });
