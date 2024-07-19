9c9b33714bdd4533ab1e1123a8cc0cd24b6aacfd
E3FxGaming <8276268+E3FxGaming@users.noreply.github.com>
2024-07-19 15:31:55 +0200

    backend: scenario test improvment

--- apps/party-time-backend-kotlin/src/test/kotlin/com/partytime/scenario/OrganizerAndParticipantScenarioTest.kt
+++ apps/party-time-backend-kotlin/src/test/kotlin/com/partytime/scenario/OrganizerAndParticipantScenarioTest.kt
@@ -4,6 +4,7 @@ import com.partytime.EVENT_NAME
 import com.partytime.OTHER_VERIFIED_EMAIL
 import com.partytime.VERIFIED_EMAIL
 import com.partytime.api.dto.event.EventCreateDTO
+import com.partytime.api.dto.event.EventDetailsDTO
 import com.partytime.api.dto.event.InvitationCreateDTO
 import com.partytime.api.dto.event.OrganizerEventDTO
 import com.partytime.api.dto.event.ParticipantEventDTO
@@ -11,18 +12,19 @@ import com.partytime.jpa.mapper.toAddressDTO
 import com.partytime.testAbstraction.PartyTimeWebTestClients
 import com.partytime.testAbstraction.ScenarioTest
 import com.partytime.testUtility.generateAddress
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.test.web.reactive.server.returnResult
 import java.time.ZonedDateTime
 
-class CreatePartyInviteUserScenarioTest @Autowired constructor(
+class OrganizerAndParticipantScenarioTest @Autowired constructor(
     private val wtc: PartyTimeWebTestClients
 ) : ScenarioTest() {
 
     @Test
-    fun testLoginCreatePartyInviteUser() {
+    fun testOrganizerAndParticipant() {
 
         val eventCreateDTO = EventCreateDTO(
             EVENT_NAME,
@@ -45,6 +47,28 @@ class CreatePartyInviteUserScenarioTest @Autowired constructor(
 
         val eventId = organizerEventDTO.eventDetailsDTO.id
 
+        //check if event in organized events
+        organizerWebTestClient
+            .get()
+            .uri("/api/host/events")
+            .exchange()
+            .expectBody(Array<EventDetailsDTO>::class.java)
+            .value { events ->
+                assertNotNull(events.firstOrNull {
+                    it.id == eventId
+                })
+            }
+
+        //get event directly
+        organizerWebTestClient
+            .get()
+            .uri("/api/host/event/$eventId")
+            .exchange()
+            .expectBody(OrganizerEventDTO::class.java)
+            .value { responseOrganizerEventDTO ->
+                assertEquals(eventId, responseOrganizerEventDTO.eventDetailsDTO.id)
+            }
+
         val invitationCreateDTO = InvitationCreateDTO(OTHER_VERIFIED_EMAIL)
 
         organizerWebTestClient
@@ -56,16 +80,35 @@ class CreatePartyInviteUserScenarioTest @Autowired constructor(
 
         val inviteeClient = wtc.authenticatedClientByEmail(OTHER_VERIFIED_EMAIL)
 
-        inviteeClient
+        val invitation = inviteeClient
             .get()
-            .uri("/api/participant/events")
+            .uri("/api/participant/event/$eventId")
+            .exchange()
+            .expectAll(
+                { spec -> spec.expectStatus().isOk },
+                { spec ->
+                    spec.expectBody(ParticipantEventDTO::class.java)
+                        .value { participantEventDTO ->
+                            assertEquals(eventId, participantEventDTO.organizedEventDetailsDTO.id)
+                        }
+                }
+            ).returnResult<ParticipantEventDTO>()
+            .responseBody
+            .single()
+            .block()!!
+
+        //organizer uninvites participant
+        organizerWebTestClient
+            .delete()
+            .uri("/api/host/event/$eventId/participants/${invitation.invitationDetailsDTO.id}")
             .exchange()
             .expectStatus().isOk
-            .expectBody(Array<ParticipantEventDTO>::class.java)
-            .value { events ->
-                assertNotNull(events.firstOrNull {
-                    it.organizedEventDetailsDTO.id == eventId
-                })
-            }
+
+        //organizer deletes event
+        organizerWebTestClient
+            .delete()
+            .uri("/api/host/event/$eventId")
+            .exchange()
+            .expectStatus().isOk
     }
 }
